954b67e8f1cc9a201cb36e225f64ce5aa39eaf76
cry <freedom@xvh.lol>
2026-02-17 03:20:24 -0500

    chore: format whitespace in createStore function

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -4,11 +4,11 @@ import { RedisStore } from "./redis";
 
 export function createStore(): PasteStore {
   const redisUrl = process.env.REDIS_URL;
-  
+
   if (redisUrl) {
     return new RedisStore(redisUrl);
   }
-  
+
   return new MemoryStore();
 }
 
